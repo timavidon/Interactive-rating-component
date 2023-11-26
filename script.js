88da704c0dd4ebb0be8507669f28f5a61de97db9
Tim Avidon <timavidon@Tims-MacBook-Air.local>
2023-11-26 14:26:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-
+const ratingCard = document.querySelector('.rating-card');
+const thanksCard = document.querySelector('.thank-you-card');
+const submitBtn = document.querySelector('.submit-btn');
+const userRating = document.querySelector('.user-rating');
 
 // Get all the rating numbers (1-5)
 const ratingNumbers = document.querySelectorAll('.rating-btn');
 
 // Wait for the user to choose a number
-for (let i = 0; i < ratingNumbers.length; i++) {
-    ratingNumbers[i].addEventListener('click', () => {
-        const chosenNumber = ratingNumbers[i].innerHTML;
-        // console.log(chosenNumber);
-        document.querySelector('.selected-rating').innerHTML = `You selected ${chosenNumber} out of 5`;
+ratingNumbers.forEach((ratingNum) => {
+    ratingNum.addEventListener('click', () => {
+        let chosenRating = 0;
+        chosenRating = ratingNum.innerHTML;
+        userRating.innerHTML = `${chosenRating}`;
     });
-}
+});
+
+submitBtn.addEventListener('click', () => {
+    ratingCard.classList.toggle('hidden');
+    thanksCard.classList.toggle('hidden');
+});
+
+// for (let i = 0; i < ratingNumbers.length; i++) {
+//     ratingNumbers[i].addEventListener('click', () => {
+//         const chosenNumber = ratingNumbers[i].innerHTML;
+//         document.querySelector('.selected-rating').innerHTML = `You selected ${chosenNumber} out of 5`;
+//     });
+// }
